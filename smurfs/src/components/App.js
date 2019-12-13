9d1220e0a891ca60700edecd34f3ccef9ec69a74
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-13 11:39:10 -0700

    app.js: refactored from class component and setup rendering of components

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,34 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+import React, {useEffect} from "react";
+import {connect} from 'react-redux';
 
-export default App;
+import {getSmurfs, postSmurf} from '../Actions';
+
+import "./App.css";
+import SmurfList from './SmurfList/SmurfList';
+import SmurfForm from './Form/SmurfForm';
+
+
+const App = props => {
+
+    useEffect(() => {
+        props.getSmurfs();
+        console.log('app props', props);
+
+    }, []);
+
+    return (
+        <div className="App">
+            <SmurfForm postSmurf={props.postSmurf}/>
+            <SmurfList smurfList={props.smurfList}/>
+        </div>
+    );
+
+};
+
+const mapStateToProps = state => {
+    return {
+        ...state
+    };
+};
+
+export default connect(mapStateToProps, {getSmurfs, postSmurf})(App);
