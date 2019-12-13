8d141465c92e04d699c03440785eccce827cd9e5
Jacob Washburn <jacobrwashburn@gmail.com>
2019-12-13 11:37:45 -0700

    action.index.js: able to pass actions and action case types from one file

---
+++ smurfs/src/Actions/index.js
@@ -0,0 +1,2 @@
+export {getSmurfs,GET_SMURF_START,GET_SMURF_SUCCESS,GET_SMURF_FAILURE} from './GetSmurfs'
+export {postSmurf,POST_SMURF_START,POST_SMURF_SUCCESS,POST_SMURF_FAILURE} from './PostSmurfs'
